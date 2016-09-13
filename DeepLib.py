1198f190b4e5d961b4129ec031afa4847b100f3e
Futen <tdk356@yahoo.com.tw>
2016-09-13 16:06:04 +0800

    gg

---
+++ DeepLib.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def normalized_image_coordinates(pixel_coords, width, height):
+    size = max(width, height)
+    p = np.empty((len(pixel_coords), 2))
+    p[:, 0] = (pixel_coords[:, 0] + 0.5 - width / 2.0) / size
+    p[:, 1] = (pixel_coords[:, 1] + 0.5 - height / 2.0) / size
+    return p
+
+def denormalized_image_coordinates(norm_coords, width, height):
+    size = max(width, height)
+    p = np.empty((len(norm_coords), 2))
+    p[:, 0] = norm_coords[:, 0] * size - 0.5 + width / 2.0
+    p[:, 1] = norm_coords[:, 1] * size - 0.5 + height / 2.0
+    return p
+
--- run_deepmatching.py
+++ run_deepmatching.py
@@ -3,6 +3,8 @@ import sys
 import subprocess
 import os
 import numpy as np
+import DeepLib as DL
+import cv2
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -39,7 +41,11 @@ if __name__ == '__main__':
         #subprocess.call(command, shell=True)
     deep_file_lst = sorted(os.listdir(match_tmp_path))
     #print deep_file_lst
+    pointRecord = {}
     for img in image_lst:
+        pointRecord[img] = []
+    for img in image_lst:
+        print img
         matches = {}
         match_lst = [x for x in deep_file_lst if x.startswith(img)]
         for match_file in match_lst:
@@ -52,13 +58,27 @@ if __name__ == '__main__':
                 A_col = int(line[0])
                 B_row = int(line[3])
                 B_col = int(line[2])
-                A_feature_id = A_col * Config.IMAGE_HEIGHT + A_row + 1
-                B_feature_id = B_col * Config.IMAGE_HEIGHT + B_row + 1
-                match_tmp.append([A_feature_id, B_feature_id])
+                A_loc = [A_row, A_col]
+                B_loc = [B_row, B_col]
+                if not A_loc in pointRecord[img]:
+                    pointRecord[img].append(A_loc)
+                if not B_loc in pointRecord[match_img]:
+                    pointRecord[match_img].append(B_loc)
+                A_id = pointRecord[img].index(A_loc)
+                B_id = pointRecord[match_img].index(B_loc)
+                match_tmp.append([A_id, B_id])
             f.close()
             matches[match_img] = np.array(match_tmp)
-        print matches
-
+        #print matches
+        tmp = np.array(pointRecord[img])
+        img_color = cv2.imread('%s/%s'%(image_path, img))
+        color_data = img_color[tmp[:,0], tmp[:,1]]
+        pointRecord[img] = DL.normalized_image_coordinates(tmp, Config.IMAGE_WIDTH, Config.IMAGE_HEIGHT)
+        pointRecord[img] = np.fliplr(pointRecord[img])
+        data = {'color':color_data, 'points':pointRecord[img], 'matches':matches}
+        np.save('%s/%s.npy'%(match_path, img), data)
+        
+        del pointRecord[img]
 
 
 
